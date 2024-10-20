ed6d2d48f9c67c8a1bc808ebe60bb46b7493917f
weiyueli7 <wel019@ucsd.edu>
2024-10-20 04:26:53 +0000

    finetune docker

--- src/finetune/finetune.py
+++ src/finetune/finetune.py
@@ -142,8 +142,8 @@ from transformers import AdamW, CLIPProcessor, CLIPModel
 # Function to parse arguments
 def parse_args():
     parser = argparse.ArgumentParser(description="Fine-tune FashionCLIP on a custom dataset")
-    parser.add_argument('--json_file', type=str, default='/home/wel019/AC215_FashionAI/src/finetune/finetune_data/final_output.json', help="Path to the dataset JSON file")
-    parser.add_argument('--image_dir', type=str, default='/home/wel019/AC215_FashionAI/src/finetune/finetune_data/AC215Images4', help="Path to the directory containing images")
+    parser.add_argument('--json_file', type=str, default='finetune_data/final_output.json', help="Path to the dataset JSON file")
+    parser.add_argument('--image_dir', type=str, default='finetune_data/AC215Images4', help="Path to the directory containing images")
     parser.add_argument('--batch_size', type=int, default=32, help="Batch size for training")
     parser.add_argument('--learning_rate', type=float, default=5e-6, help="Learning rate for optimizer")
     parser.add_argument('--epochs', type=int, default=3, help="Number of epochs to train for")
